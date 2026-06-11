10ab0e88339385042cc07c5deb3373f510a9b7c1
gitaarwerk <gitaarwerk@users.noreply.github.com>
2026-06-11 21:15:39 +0000

    deploy: 8b41d24c07030cc542fe10c22a5d9388ba405f0e

--- _next/static/chunks/3isd-1_wjecnw.js
+++ _next/static/chunks/3isd-1_wjecnw.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/3l5--bz1bm8td.js",
   "static/chunks/2eyrpya3hnoq9.js",
   "static/chunks/189cyo79icvwq.js",
-  "static/chunks/38aau6ocawm4_.js",
+  "static/chunks/22nrgk3vjagw2.js",
   "static/chunks/39pa6dxk9rme2.css",
-  "static/chunks/turbopack-06t4hb3vqs28a.js"
+  "static/chunks/turbopack-1mvyrzoxa9f2m.js"
 ])
